40baa253faf0ab340400c10cd78bd7d56fcdf8a4
Donjuanplatinum <donplat@barrensea.org>
2026-03-29 00:35:35 +0800

    adding serial support

---
+++ crates/rkos/src/console.rs
@@ -0,0 +1,18 @@
+use core::fmt;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::console::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($fmt:expr) => ($crate::print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    rkos_arch::x86::serial::_print(args);
+}
